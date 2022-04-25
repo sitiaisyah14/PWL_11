080259d85161c4508ddeda250b9e516bab9d49a3
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-04-25 12:51:00 +0700

    Praktikum 2: CRUD dalam RESTful API (CRUD di Postman)

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,5 +25,8 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
-})
+    Route::apiResource('/todos', TodoController::class);
+});
+
+
 
